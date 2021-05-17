28fd8df585c56e68c4fcb3896747c09d1935c4b0
gab <19020303@myrp.edu.sg>
2021-05-17 12:56:45 +0800

    done with Song.java

---
+++ app/src/main/java/sg/edu/rp/c346/id19008424/p04problemstatement/DBHelper.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c346.id19008424.p04problemstatement;
+
+public class DBHelper {
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id19008424/p04problemstatement/Song.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.id19008424.p04problemstatement;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private int id;
+    private String title;
+    private String singers;
+    private int year;
+    private int stars;
+
+    public Song(String title, String singers, int year, int stars) {
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+}
