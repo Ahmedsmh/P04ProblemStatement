ae4f0e7a6883ee9ed954171944b4e662c87f8a55
Ahmed <19008424@myrp.edu.sg>
2021-05-17 12:37:12 +0800

    Created activities

---
+++ app/src/main/java/sg/edu/rp/c346/id19008424/p04problemstatement/SecondActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.id19008424.p04problemstatement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id19008424/p04problemstatement/ThirdActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19008424.p04problemstatement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
