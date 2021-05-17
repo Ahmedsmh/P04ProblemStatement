3024f95b4f3959aec20a0867737195a09534f8c0
19036308 <19036308@myrp.edu.sg>
2021-05-17 14:24:17 +0800

    done 2nd Act other than show all song btn

--- app/src/main/java/sg/edu/rp/c346/id19008424/p04problemstatement/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19008424/p04problemstatement/SecondActivity.java
@@ -1,13 +1,63 @@
 package sg.edu.rp.c346.id19008424.p04problemstatement;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
+
+    Button btn5Stars;
+    ListView lv;
+    SongAdapter sa;
+    ArrayList<Song> alSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        lv = findViewById(R.id.lvSongs);
+        DBHelper dbSong = new DBHelper(SecondActivity.this);
+        alSong = dbSong.getAllSongs();
+        btn5Stars = findViewById(R.id.btn5StarsSong);
+
+        btn5Stars.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if()
+
+            }
+        });
+        lv.setAdapter(sa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Song target = alSong.get(position);
+                Intent i = new Intent(SecondActivity.this, ThirdActivity.class);
+                i.putExtra("data", target);
+                startActivityForResult(i, 9);
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == 9){
+//            btnRetrieve.performClick();
+        }
+    }
+
 }
